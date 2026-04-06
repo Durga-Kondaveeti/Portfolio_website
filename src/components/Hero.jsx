9892ee36585bbe6cd0dcd3f22576b3b3645a0144
Durga-kondaveeti <durgashankarkondaveeti@gmail.com>
2026-04-06 04:21:40 -0400

    Update: Configured custom domain durgakondaveeti.site with CNAME and package.json update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -68,7 +68,7 @@ const Hero = () => {
             <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
             <span className="relative inline-flex rounded-full h-2.5 w-2.5 bg-green-500"></span>
           </span>
-          Currently seeking Fall 2026 SDE Roles
+          Open to Opportunities
         </motion.div>
 
         {/* Title */}
